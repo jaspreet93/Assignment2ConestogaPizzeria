11393d1bc756d27fa13fd9d61ba39e23c41053ea
jaspreet93 <erjaspreetkaur.20@gmail.com>
2016-03-18 16:36:49 +0000

    price is coming automaticlly

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,14 @@
 class Order < ActiveRecord::Base
+    validates :Size, presence: true
+    
+    before_save do
+      if self.Size == "small"
+        self.Price = 2.00
+      elsif self.Size = "medium"
+        self.Price = 2.00
+      else
+        self.Price = 2.00
+      end
+    end
+  
 end
